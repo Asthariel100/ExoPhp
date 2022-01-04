77dfb1d945d6ff50d66db529d64f3e82b90508f9
Yohann <yohann.fallas25@gmail.com>
2022-01-04 16:46:47 +0100

    Html dans Php <3

--- contact.php
+++ contact.php
@@ -4,6 +4,17 @@
   <title>PHP Test</title>
  </head>
  <body>
+ <main>
+        <form action="http://localhost:3000/contact.php" method="get">
+<input type="text" name="nom" placeholder="nom">
+<input type="text" name="prenom"placeholder="prenom">
+<input type="text" name="mail"placeholder="mail">
+<input type="text"name="tel"placeholder="tel">
+<input type="text"name="adresse"placeholder="adresse">
+<button placeholder="annuler" type="reset">Annuler</button>
+<button type="submit"name="submit">Envoyer</button>
+        </form>
+    </main>
 <?php
 
 $nom = $_GET["nom"];
@@ -23,7 +34,7 @@ $Err = "";
         if(!$Err)  {
             print_r($formdata);
         }
-        
+
 ?>
 </body>
 </html>
