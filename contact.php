481ccbaaaf92e80b764329074485e34bee3e4626
Yohann <yohann.fallas25@gmail.com>
2022-01-05 11:33:06 +0100

    modif Vincent

--- contact.php
+++ contact.php
@@ -5,23 +5,23 @@
  </head>
  <body>
  <main>
-        <form action="http://localhost:3000/contact.php" method="get">
-<input type="text" name="nom" placeholder="nom">
+        <form action="http://localhost:3000/contact.php" method="post">
+<input minlength="3"type="text" name="nom" placeholder="nom">
 <input type="text" name="prenom"placeholder="prenom">
-<input type="text" name="mail"placeholder="mail">
-<input type="text"name="tel"placeholder="tel">
-<input type="text"name="adresse"placeholder="adresse">
+<input type="email" name="mail"placeholder="mail">
+<input maxlenght="10" minlength="10"pattern="[0-9]{1-10}" type="tel"name="tel"placeholder="tel">
+<input minlength="10" name="adresse"placeholder="adresse">
 <button placeholder="annuler" type="reset">Annuler</button>
 <button type="submit"name="submit">Envoyer</button>
         </form>
     </main>
 <?php
 
-$nom = $_GET["nom"];
-$prenom = $_GET["prenom"];
-$mail = $_GET["mail"];
-$tel = $_GET["tel"];
-$adresse = $_GET["adresse"];
+$nom = $_POST["nom"];
+$prenom = $_POST["prenom"];
+$mail = $_POST["mail"];
+$tel = $_POST["tel"];
+$adresse = $_POST["adresse"];
 $formdata = [$nom,$prenom,$mail,$tel,$adresse];
 $Err = "";
     for($i=0;$i<=4;$i++){
